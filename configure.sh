ea37ffe701891669044f152a2b176b6d541f58ae
MikeB1124 <balianmichael@gmail.com>
2025-04-27 08:45:36 -0700

    add corrections

--- configure.sh
+++ configure.sh
@@ -1,10 +1,10 @@
 
 apt update -y
-apt install -y python3 git vim python3.12-venv python3-pip
+apt install -y python3 git vim python3.10-venv python3-pip
 
 python3 -m venv venv
 source venv/bin/activate
-pip install -r requirements.txt
+pip install -r requirments.txt
 
 tar -xvf voices.tar.gz
 
@@ -13,5 +13,5 @@ wget wget https://github.com/rhasspy/piper/releases/download/v1.2.0/piper_amd64.
 tar -xvf piper_amd64.tar.gz
 sudo mv piper/piper /usr/bin/piper
 sudo cp piper/*.so* /usr/local/lib/
-sudo ldconfig
 sudo cp -r piper/espeak-ng-data /usr/share/
+sudo ldconfig
